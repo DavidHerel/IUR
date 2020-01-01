4111812b71b624d108f301487cfd2a1535571b5a
David Herel <hereldav@fel.cvut.cz>
2020-01-01 13:23:23 +0100

    Satnik termpaper progress - funguje pridavani obleceni

---
+++ TermPaper/Aplikace/Satnik/Satnik/ViewModels/MainViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using Satnik.Support;
+
+namespace Satnik.ViewModels
+{
+    public class MainViewModel : ViewModelBase
+    {
+        public ObservableCollection<ObleceniCustomCardViewModel> ObleceniCards { get; set; } = new ObservableCollection<ObleceniCustomCardViewModel>();
+
+        public MainViewModel()
+        {
+            
+            ObleceniCards.Add(new ObleceniCustomCardViewModel(this, "tricko", "zelena", "sportovni"));
+
+        }
+
+    }
+}
---
+++ TermPaper/Aplikace/Satnik/Satnik/ViewModels/ObleceniCustomCardViewModel.cs
@@ -0,0 +1,48 @@
+﻿using Satnik.Support;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Satnik.ViewModels
+{
+    public class ObleceniCustomCardViewModel:ViewModelBase
+    {
+
+        public ObleceniCustomCardViewModel(MainViewModel mainViewModelReference, string druh, string barva, string kategorie)
+        {
+            _mainViewModelReference = mainViewModelReference;
+            Druh = druh;
+            Barva = barva;
+            Kategorie = kategorie;
+        }
+
+        private RelayCommand _removeCityCommand;
+        private MainViewModel _mainViewModelReference;
+        private string _druh;
+        private string _barva;
+        private string _kategorie;
+
+        public string Druh { get { return _druh; } set { _druh = value; OnPropertyChanged(); } }
+
+        public string Barva { get { return _barva; } set { _barva = value; OnPropertyChanged(); } }
+
+
+        public string Kategorie { get { return _kategorie; } set { _kategorie = value; OnPropertyChanged(); } }
+
+        public RelayCommand DeleteCity
+        {
+            get
+            {
+                return _removeCityCommand ?? (_removeCityCommand = new RelayCommand(DeleteCityItem));
+            }
+        }
+
+        private void DeleteCityItem(object obj)
+        {
+            _mainViewModelReference.ObleceniCards.Remove(this);
+        }
+
+    }
+}
